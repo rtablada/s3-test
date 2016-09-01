ca6b2399bc881f5f9395963ecec5ab8710de36ca
Ryan Tablada <ryan.tablada@gmail.com>
2016-09-01 08:02:07 -0500

    start s3-test app

---
+++ config/filesystems.js
@@ -0,0 +1,37 @@
+'use strict'
+
+const Env = use('Env')
+const Helpers = use('Helpers')
+
+module.exports = {
+
+  default: 'public',
+
+  public: {
+    driver: 'local',
+    root: Helpers.publicPath('uploads'),
+    options: {
+      encoding: 'utf8'
+    }
+  },
+
+  protected: {
+    driver: 'local',
+    root: Helpers.storagePath('app'),
+    options: {
+      encoding: 'utf8'
+    }
+  },
+
+  s3: {
+    driver: 'pkgcloud',
+    bucket: Env.get('S3_BUCKET'),
+    options: {
+      provider: 'amazon',
+      keyId: Env.get('S3_KEY'), // access key id
+      key: Env.get('S3_SECRET'), // secret key
+      region: Env.get('S3_REGION'), // region
+    }
+  }
+
+}
